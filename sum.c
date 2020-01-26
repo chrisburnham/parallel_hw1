4225aa2e96447cb3b40782d462f84dc95f69d130
Chris Burnham <cburnham@greensea.com>
2020-01-26 17:12:20 -0500

    100000

--- sum.c
+++ sum.c
@@ -12,8 +12,8 @@
 
 #include "Timer.h"
 
-#define SIZE       100000000  // The size of the array to sum.
-#define ITERATIONS 100        // The number of times we'll do the summation for timing purposes.
+#define SIZE       100000  // The size of the array to sum.
+#define ITERATIONS 100000        // The number of times we'll do the summation for timing purposes.
 
 // -----------
 // Serial Sums
